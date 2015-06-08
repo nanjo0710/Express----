6fc5d90647f3915be136313c3429106f4d4d75ab
Takanori Nanjo <nanjo0710@gmail.com>
2015-06-08 17:51:27 +0900

    Indexを目次化

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,23 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function (req, res) {
+
+    var pages = [
+    { name: 'users', path: 'users', memo: 'user.list' },
+    { name: 'ab?cd', path: 'abcd', memo: 'will match acd and abcd' },
+    { name: 'ab+cd', path: 'abbcd', memo: 'will match abcd, abbcd, abbbcd, and so on' },
+    { name: 'ab*cd', path: 'abxcd', memo: 'will match abcd, abxcd, abRABDOMcd, ab123cd, and so on' },
+    { name: 'ab(cd)?e', path: 'abe', memo: 'will match /abe and /abcde' },
+    { name: 'ab?cd', path: 'acd', memo: 'will match acd and abcd' },
+    { name: 'abcd', path: 'ab?cd', memo: 'will match acd and abcd' },
+    { name: 'abcd', path: 'ab?cd', memo: 'will match acd and abcd' },
+    { name: 'abcd', path: 'ab?cd', memo: 'will match acd and abcd' },
+    { name: 'abcd', path: 'ab?cd', memo: 'will match acd and abcd' },
+    { name: 'abcd', path: 'ab?cd', memo: 'will match acd and abcd' },
+    { name: 'abcd', path: 'ab?cd', memo: 'will match acd and abcd' },
+    { name: 'abcd', path: 'ab?cd', memo: 'will match acd and abcd' },
+    { name: 'abcd', path: 'ab?cd', memo: 'will match acd and abcd' }];
+
+    res.render('index', { title: 'Express', pages: pages });
 };
